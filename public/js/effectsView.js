9381fa7a680dbe968a5087e43f1dd466026ee387
Simon Bernoud <simon.bernoud@gmail.com>
2021-04-26 19:21:39 +0200

    probleme clic map ok + bdd

--- public/js/effectsView.js
+++ public/js/effectsView.js
@@ -7,19 +7,27 @@ function closeMenu(menu) {
     menu.className = "absolute inset-y-0 h-full z-5000 transform -translate-x-full transition duration-250 ease-in-out";
 }
 
-
-//PANEL
+///////////////////
+//     PANEL     //
+///////////////////
 let panelOpen = false;
 
-function closePanel() {
+function closePanel(panel) {
     panel.classList.toggle("translate-x-full");
-    panel.querySelector("#latitude").value = null;
-    panel.querySelector("#longitude").value = null;
+    
+    if(panel == document.querySelector('panelSite')) {
+        panel.querySelector("#latitude").value = null;
+        panel.querySelector("#longitude").value = null;
+    }
+
+    panel.querySelectorAll(".newPoint").forEach((point) => {
+        point.remove();
+    })
 
     panelOpen = false;
 }
 
-function retractPanel() {
+function retractPanel(panel) {
     if(panel.classList.contains('translate-x-retracted'))
         panel.querySelector("#retract_button").innerHTML = ">";
     else
@@ -27,14 +35,37 @@ function retractPanel() {
     panel.classList.toggle("translate-x-retracted");
 }
 
-function openPanel(lat, long) {
+function openPanel(panel, lat, long) {
     if(panelOpen)
-        closePanel();
+        closePanel(panel);
 
     panel.classList.toggle("translate-x-full");
     
-    panel.querySelector("#latitude").value = lat;
-    panel.querySelector("#longitude").value = long;
+    if(lat != null) 
+        panel.querySelector("#latitude").value = lat;
+    if(long != null)
+        panel.querySelector("#longitude").value = long;
 
     panelOpen = true;
+}
+
+/**
+ * Fonction qui permet de rajouter un point de délimitation d'un chantier
+ */
+function addPoint() {
+    var parent = document.getElementById("position");
+    var button = document.getElementById("contentButtonAddPoint");
+   
+    var newDiv = document.createElement("div");
+    newDiv.classList.toggle("text-center");
+    newDiv.classList.toggle("newPoint");
+
+    newDiv.innerHTML = `
+        <label>Latitude :</label>
+        <input class="rounded-lg" type="number" id="latitude" required>
+
+        <label>Longitude :</label>
+        <input class="rounded-lg" type="number" id="longitude" required>`;
+
+        parent.insertBefore(newDiv, button);
 }
