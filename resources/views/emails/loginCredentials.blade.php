ced7f1ca3b2bc2ab6d1ebc2750e5cdef56e5a700
Nicolas.C <comcomnico@gmail.com>
2021-08-18 19:26:14 +0200

    minor change

--- resources/views/emails/loginCredentials.blade.php
+++ resources/views/emails/loginCredentials.blade.php
@@ -8,9 +8,9 @@ Voici vos identifiants de connexion :
 Login : {{ $data['email'] }}  
 Mot de passe : {{ $password }}
 
-## <p style="color: #FF0000">Conservez précieusement ces identifiants. Vous pourrez par la suite modifier votre mot de passe dans la gestion de votre compte.</p>
+<p style="color: #FF0000">**Conservez précieusement ces identifiants. Vous pourrez par la suite modifier votre mot de passe dans la gestion de votre compte.**</p>
 
-## <p style="color: #FF0000">Assurez-vous d'avoir valider votre compte aupréalable grâce à un autre mail qui vous a été envoyé par GéoRapport. Si vous ne validez pas votre compte, il sera impossible pour vous de vous connecter.</p>
+<p style="color: #FF0000">**Assurez-vous d'avoir valider votre compte aupréalable grâce à un autre mail qui vous a été envoyé par GéoRapport. Si vous ne validez pas votre compte, il sera impossible pour vous de vous connecter.**</p>
 
 Merci,  
 L'équipe de développement de {{ config('app.name') }}
